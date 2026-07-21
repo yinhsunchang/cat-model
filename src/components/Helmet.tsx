b955d0383798325971d506be5c16f639f8cde7b3
yinhsunchang <yinhsunchang@gmail.com>
2026-07-21 10:58:25 +0200

    refactor: move i18n files from src to i18n directory

--- src/components/Helmet.tsx
+++ src/components/Helmet.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
-import "../i18n";
+import "../i18n/i18n";
 
 const MyHelmet = () => {
   const { t, i18n } = useTranslation();
