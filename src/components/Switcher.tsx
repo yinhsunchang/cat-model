32d97f20a7ab9e229f50da68403054b1759b0cd1
yinhsunchang <yinhsunchang@gmail.com>
2026-07-23 11:19:46 +0200

    refactor: relocate language switcher from floating button to navbar and sidebar

--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -2,20 +2,24 @@ import { useTranslation } from "react-i18next";
 
 function Switcher() {
   const { i18n } = useTranslation();
-  
+
   const changeLanguage = (lng: string) => {
-  i18n.changeLanguage(lng);
-  document.documentElement.lang = lng;
+    i18n.changeLanguage(lng);
+    document.documentElement.lang = lng;
   };
 
   return (
     <>
-    <div style={{ position: "fixed", bottom: 10, right: 10, zIndex: 1000 }}>
-    <button className="button black" onClick={() => changeLanguage("en")}>EN</button>
-    <button className="button black" onClick={() => changeLanguage("fr")}>FR</button>
-    </div>
+      <div>
+        <button className="button center" onClick={() => changeLanguage("en")}>
+          EN
+        </button>
+        <button className="button center" onClick={() => changeLanguage("fr")}>
+          FR
+        </button>
+      </div>
     </>
   );
 }
 
-export default Switcher
+export default Switcher;
