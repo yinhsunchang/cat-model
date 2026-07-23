2ebb861e0e1e5c32f375b79484304a18f9de7baf
yinhsunchang <yinhsunchang@gmail.com>
2026-07-23 11:20:06 +0200

    refactor: move language switcher to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Switcher from "../components/Switcher.tsx";
 import { useTranslation } from "react-i18next";
 
 function Navbar() {
@@ -5,14 +6,60 @@ function Navbar() {
 
   return (
     <div className="top hide-large hide-medium">
-    <div className="bar black opacity hover-opacity-off center small">
-      <a href="#home" className="bar-item button" style={{width:'25%'}}>{t("nav.home")}</a>
-      <a href="#about" className="bar-item button" style={{width:'25%'}}>{t("nav.about")}</a>
-      <a href="#photos" className="bar-item button" style={{width:'25%'}}>{t("nav.photos")}</a>
-      <a href="#contact" className="bar-item button" style={{width:'25%'}}>{t("nav.contact")}</a>
-    </div>
+      <div className="bar black opacity hover-opacity-off center small">
+        <a
+          href="#home"
+          title={t("nav.home")}
+          className="bar-item button"
+          style={{ width: "20%" }}
+        >
+          <i className="fa fa-home xlarge"></i>
+        </a>
+        <a
+          href="#about"
+          title={t("nav.about")}
+          className="bar-item button"
+          style={{ width: "20%" }}
+        >
+          <i className="fa fa-paw xlarge"></i>
+        </a>
+        <a
+          href="#photos"
+          title={t("nav.photos")}
+          className="bar-item button"
+          style={{ width: "20%" }}
+        >
+          <i className="fa fa-photo xlarge"></i>
+        </a>
+        <a
+          href="#contact"
+          title={t("nav.contact")}
+          className="bar-item button"
+          style={{ width: "20%" }}
+        >
+          <i className="fa fa-envelope xlarge"></i>
+        </a>
+        <div
+          className="bar-item dropdown-hover button"
+          style={{ width: "20%" }}
+          title={t("nav.language")}
+        >
+          <i className="fa fa-globe xlarge"></i>
+          <div
+            className="dropdown-content"
+            style={{
+              width: "20%",
+              position: "relative",
+              zIndex: 100,
+              minWidth: 80,
+            }}
+          >
+            <Switcher />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Navbar
+export default Navbar;
